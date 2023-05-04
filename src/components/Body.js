c7bbdf50ad391bd7927cea19cb94383beeaf7e42
Ehaley25 <elijahhaley99@gmail.com>
2023-05-03 22:29:04 -0700

    quick header build. will add on soon

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Body = () =>{
+    return(
+        <div>
+            <p>Im the body!</p>
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Header = () =>{
+    return(
+        <div className='header'>
+            <div>
+                <ul className='page-links'>
+                    <li>Home</li>
+                    <li>Catalog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='icons'>
+                <h1>Test</h1>
+                <h1>Test</h1>
+            </div>
+        </div>
+    )
+}
+export default Header
+
+// icons is a name for now will change when ready to deploy, just need to get some code put out
