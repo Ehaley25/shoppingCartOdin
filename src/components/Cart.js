0c42d2985ff67944dd0054e867216873733c5d45
Ehaley25 <elijahhaley99@gmail.com>
2023-05-21 21:26:11 -0700

    getting the cart working- want to finish on 5/22

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,13 +6,30 @@ const Cart = () =>{
 
     const {cart , setCart} = useContext(MyContext)
 
+    function cartItems(){
+        return(
+            cart.map((item , key) =>{
+                return(
+                    <div key={key} className="checkout-Layout">
+                        <img src={item.img} alt="This is a test" />
+                        <div className="checkout-details">
+                            <p>{item.title}</p>
+                            <p>{item.price}</p>
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
+
+    cartItems()
     console.log(cart)
     return(
-        <div className="border-top">
-            <div className="container ">
+        <div className="border-top mid-screen">
+            <div className="checkOut ">
                 {
                     !cart.length ? <p>Im an empty Cart</p> :
-                    <p>{cart.length}</p>
+                    cartItems()
                 }
             </div>
         </div>
